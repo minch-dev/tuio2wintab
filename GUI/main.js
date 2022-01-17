504e3fd82e77b13fdc7434005d14c59ac433b118
minch-yoda <minchenko.dev@gmail.com>
2022-01-17 17:20:51 +0300

    display metrics visual representation implemented

--- GUI/main.js
+++ GUI/main.js
@@ -1,35 +1,54 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	// Create the browser window.
+	const { screen } = require('electron');
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true
+		}
+	});
+	
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html');
+	
+	mainWindow.webContents.once('dom-ready', () => {
+		displaysChanged();
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	screen.on('display-metrics-changed', (event, display, changedMetrics) =>
+	{
+		displaysChanged();
+		
+	});
+	function displaysChanged(){
+		mainWindow.webContents.send('displays-changed',
+			{all:screen.getAllDisplays(), main:screen.getPrimaryDisplay()}
+		);
+		
+	}
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+	createWindow();
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
--- GUI/renderer.js
+++ GUI/renderer.js
@@ -4,3 +4,34 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+    const electron = require('electron');
+    const ipcRenderer = electron.ipcRenderer;
+	var displays = {};
+    ipcRenderer.on('displays-changed', function (event, d) {
+		displays = d;
+		drawWorkspace();
+    });
+	function drawWorkspace(){
+		var workspace = document.getElementsByTagName('workspace')[0];
+		var html = '';
+		var minX = 0;
+		var minY = 0;
+		var maxX = 0;
+		var maxY = 0;
+		for(d=0;d<displays.all.length;d++){
+			var dis = displays.all[d];
+			var x=dis.bounds.x;
+			var y=dis.bounds.y;
+			var w=dis.bounds.width;
+			var h=dis.bounds.height;
+			var x2=x+w;
+			var y2=y+h;
+			if(x<minX){minX=x;}
+			if(y<minY){minY=y;}
+			if(x2>maxX){maxX=x2;}
+			if(y2>maxY){maxY=y2;}
+			html += '<display index="'+d+'" class="'+(dis.id == displays.main.id ? 'main' : '')+'" style="left:'+x+'px; top:'+y+'px; width:'+dis.bounds.width+'px; height:'+dis.bounds.height+'px;"><d style="transform:rotate('+dis.rotation+'deg);">'+(d+1)+'</d></display>';
+		}
+		workspace.innerHTML = html;
+		workspace.style = "top:"+(-minY)+"px; left:"+(-minX)+"px;width:"+(maxX)+"px;height:"+(maxY)+"px;";
+	}
